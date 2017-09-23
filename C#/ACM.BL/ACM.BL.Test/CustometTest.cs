e1901f9efb51c0c4dc9787c949166f2066ecb3cb
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-09-23 21:20:55 +0200

    Add a new C# customer management project
    PS - OOP Fundamentals in C#

---
+++ C#/ACM.BL/ACM.BL.Test/CustometTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BL.Test
+{
+    [TestClass]
+    public class CustometTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ C#/ACM.BL/ACM.BL/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName => LastName + ", " + FirstName;
+    }
+}
